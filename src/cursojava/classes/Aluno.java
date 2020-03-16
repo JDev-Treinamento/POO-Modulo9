e949302d1669af2e879db8c11c0254afe58db1da
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2020-03-15 21:19:38 -0300

    Aula 06 - Primeira Classe e Objeto Aluno

---
+++ src/cursojava/classes/Aluno.java
@@ -0,0 +1,19 @@
+package cursojava.classes;
+
+/**
+ * Classe que representa um Aluno
+ */
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+}
---
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+
+	/**
+	 * Método padrão que executa os programas escritos em Java
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
