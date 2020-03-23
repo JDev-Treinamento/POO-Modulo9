4ee771d9fed8dd64540a5ca57cffd3ff30eab932
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2020-03-22 21:28:08 -0300

    Aula 14 - Entrada de dados com o teclado

--- src/cursojava/executavel/PrimeiraClasseJava.java
+++ src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,5 +1,7 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
@@ -12,57 +14,53 @@ public class PrimeiraClasseJava {
 		// new Aluno() é uma instância da classe Aluno (criação do objeto)
 		// aluno1 é uma referência para o objeto
 		
-		System.out.println("===================== ALUNO 1 =======================");		
-		Aluno aluno1 = new Aluno(); // João
+		// Entrada de dados com o teclado (Dados do aluno)
+		String nome = JOptionPane.showInputDialog("Digite o nome do aluno:");
+		String idade = JOptionPane.showInputDialog("Digite a idade do aluno:");
+		String dataNascimento = JOptionPane.showInputDialog("Digite a data de nascimento do aluno:");
+		String rg = JOptionPane.showInputDialog("Digite o RG do aluno:");
+		String cpf = JOptionPane.showInputDialog("Digite o CPF do aluno:");
+		String nomeMae = JOptionPane.showInputDialog("Digite o nome da mãe do aluno:");
+		String nomePai = JOptionPane.showInputDialog("Digite o nome do pai do aluno:");
+		String dataMatricula = JOptionPane.showInputDialog("Digite a data da matrícula do aluno:");
+		String serieMatriculado = JOptionPane.showInputDialog("Digite a série do aluno:");
+		String nomeEscola = JOptionPane.showInputDialog("Digite o nome da escola do aluno:");
 		
-		//Aluno aluno1 = null; -> Declaração que lança a exceção 'NullPointerException'
+		// Entrada de dados com o teclado (Notas do aluno)
+		String nota1 = JOptionPane.showInputDialog("Digite o valor da 1º nota:");
+		String nota2 = JOptionPane.showInputDialog("Digite o valor da 2º nota:");
+		String nota3 = JOptionPane.showInputDialog("Digite o valor da 3º nota:");
+		String nota4 = JOptionPane.showInputDialog("Digite o valor da 4º nota:");		
+		
+		System.out.println("===================== ALUNO 1 =======================");	
+		Aluno aluno1 = new Aluno(); // João		
 		
 		// Dados do aluno1
-		aluno1.setNome("João da Silva");
-		aluno1.setIdade(50);
-		aluno1.setDataNascimento("18/10/1987");
-		aluno1.setRegistroGeral("44554.4544.545");
-		aluno1.setNumeroCpf("54544.545454.5454");
-		aluno1.setNomeMae("Shirlei");
-		aluno1.setNomePai("Antonio");
-		aluno1.setDataMatricula("10/01/2019");
-		aluno1.setSerieMatriculado("5");
-		aluno1.setNomeEscola("Escola JDEV Treinamento");
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setRegistroGeral(rg);
+		aluno1.setNumeroCpf(cpf);
+		aluno1.setNomeMae(nomeMae);
+		aluno1.setNomePai(nomePai);
+		aluno1.setDataMatricula(dataMatricula);
+		aluno1.setSerieMatriculado(serieMatriculado);
+		aluno1.setNomeEscola(nomeEscola);
 		
 		// Notas do aluno1
-		aluno1.setNota1(90);
-		aluno1.setNota2(80.8);
-		aluno1.setNota3(70.9);
-		aluno1.setNota4(90.7);
+		aluno1.setNota1(Double.parseDouble(nota1));
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));		
 		
+		// Informações do aluno, impressas no console
 		System.out.println("Nome do aluno 1 é: " + aluno1.getNome());
 		System.out.println("Idade do aluno 1 é: " + aluno1.getIdade());
+		System.out.println("Nome da mãe do aluno 1 é: " + aluno1.getNomeMae());
 		System.out.println("Nascimento do aluno 1 é: " + aluno1.getDataNascimento());
 		System.out.println("Média das notas do aluno 1 é: " + aluno1.getMediaNota());
 		System.out.println("Resultado BOOLEAN: " + (aluno1.getAlunoAprovadoBoolean() ? "Aprovado" : "Reprovado"));
 		System.out.println("Resultado STRING: " + aluno1.getAlunoAprovadoString());
-		// ======================================================================
-		
-		System.out.println("===================== ALUNO 2 =======================");		
-		Aluno aluno2 = new Aluno(); // Pedro
-		
-		// Dados do aluno2
-		aluno2.setNome("Pedro");
-		aluno2.setIdade(40);
-		aluno2.setDataNascimento("10/10/1970");
-		
-		System.out.println("Nome do aluno 2 é: " + aluno2.getNome());
-		System.out.println("Idade do aluno 2 é: " + aluno2.getIdade());
-		System.out.println("Nascimento do aluno 2 é: " + aluno2.getDataNascimento());
-		System.out.println("=====================================================");
-		// =====================================================================
-		
-		Aluno aluno3 = new Aluno(); // Alex
-		
-		Aluno aluno4 = new Aluno("Maria");
-		
-		Aluno aluno5 = new Aluno("José", 50);
-
 	}
 
 }
